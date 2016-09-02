b6ce5f2031db107d263842b5227b193188396537
whereistanya <tanya.reilly@gmail.com>
2016-09-02 19:27:11 -0400

    Create xmonad.hs

---
+++ xmonad.hs
@@ -0,0 +1,48 @@
+-- http ://www.haskell.org/haskellwiki/Xmonad/Config_archive
+--
+-- test correctness with 'xmonad --recompile'; reload with 'xmonad --restart'
+import XMonad
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.Combo
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.TwoPane
+import XMonad.Layout.ResizableTile
+import System.IO
+
+myManageHook = composeAll
+    [ className =? "Gimp"      --> doFloat
+    , className =? "Vncviewer" --> doFloat
+    ]
+
+-- avoidStruts: leave space for taskbars
+-- 1: how many windows in main window
+-- 3/100: amount to resize while resizing
+-- 1/2: if positive, the fraction of the screen that the main window
+--       should occupy, but if negative, the absolute value other columns
+--       should occupy.
+
+myLayout = avoidStruts (ThreeCol 1 (3/100) (1/2) ||| ThreeCol 2 (3/100) (1/2) ||| Full ||| Tall 1 (3/100) (1/2))
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/tanya/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> myManageHook
+                        <+> manageHook defaultConfig
+        , layoutHook = myLayout
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , terminal = "xterm"
+        }
+        `additionalKeys`
+        [ ((0, xK_F7), spawn "/usr/bin/gnome-screensaver-command -a")
+        , ((0, xK_F8), spawn "/usr/bin/google-chrome")
+        ]
+
