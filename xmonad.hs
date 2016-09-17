db6bd8ece10f4cade2b22a5e9d20de4dad6179e2
whereistanya <tanya.reilly@gmail.com>
2016-09-17 15:25:29 -0400

    link for pulse-volume

--- xmonad.hs
+++ xmonad.hs
@@ -47,6 +47,7 @@ main = do
         `additionalKeys`
         [ ((0, xK_F7), spawn "/usr/bin/gnome-screensaver-command -a")
         , ((0, xK_F8), spawn "/usr/bin/chromium-browser")
+        -- Map volume control keys. Uses https://github.com/bchurchill/xmonad-pulsevolume
         , ((0, 0x1008FF12), spawn "/home/tanya/pulse-volume.sh toggle")
         , ((0, 0x1008FF13), spawn "/home/tanya/pulse-volume.sh increase")
         , ((0, 0x1008FF11), spawn "/home/tanya/pulse-volume.sh decrease")
